34c021a63b68de11317f4e46ae41e6ed668b577a
Ahmed Bouzenada <ahmed.bouzenada@univ-constantine2.dz>
2024-03-07 16:56:46 +0100

    Corrected Error in app template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,26 +35,12 @@
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('payments.index') }}">
-                        <i class="fas fas fa-landmark mr-2"></i>Debts
+                        <i class="fas fa-landmark mr-2"></i>Debts
                     </a>
                 </li>
                 <!-- Add more navigation links as needed -->
             </ul>
         </div>
-<body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="">MoneyMaster</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('clients.index') }}">Clients</a>
-            </li>
-            <!-- Add more navigation links as needed -->
-        </ul>
     </div>
 </nav>
 
