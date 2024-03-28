fc4fa600c20c8c85bb49ec06bfeeac9a38a96663
Ahmed Bouzenada <ahmed.bouzenada@univ-constantine2.dz>
2024-03-28 14:14:29 +0100

    Added Close and Confirm Buttons

---
+++ resources/js/Components/Shared/Buttons/CloseButton.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import {Link} from "@inertiajs/vue3";
+
+defineProps({
+    url: String
+})
+</script>
+
+<template>
+    <Link :href="url" class="btn btn-secondary me-2"><i class="fa-solid fa-xmark me-2"></i>Close</Link>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/Shared/Buttons/ConfirmButton.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+    form: Object,
+    text: String
+})
+</script>
+
+<template>
+    <button type="submit" class="btn btn-success me-2" :disabled="form.processing"><i
+        class="fa-solid fa-check me-2"></i>{{ text }}
+    </button>
+</template>
+
+<style scoped>
+
+</style>
