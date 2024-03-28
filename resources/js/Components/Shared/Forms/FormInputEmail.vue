424dedb6168e8c8bb6b15c0948bf78c1c328fcf8
Ahmed Bouzenada <ahmed.bouzenada@univ-constantine2.dz>
2024-03-28 14:15:04 +0100

    Added FormInputs Text, Date and Email

---
+++ resources/js/Components/Shared/Forms/FormInputEmail.vue
@@ -0,0 +1,37 @@
+<script setup>
+import {computed} from "vue";
+
+let props = defineProps({
+    form: Object,
+    id: String,
+    title: String,
+    modelValue: String
+})
+const emit = defineEmits(['update:modelValue'])
+const model = computed({
+    get() {
+        return props.modelValue
+    },
+    set(value) {
+        emit('update:modelValue', value)
+    },
+});
+
+</script>
+
+<template>
+    <label :for="id" class="form-label">{{ title }}</label>
+    <input
+        type="email"
+        class="form-control"
+        v-model="model"
+        :id="id"
+        :name="id"
+        :class="{ 'is-invalid': form.errors[id] }"
+    >
+    <div v-if="form.errors[id]" class="invalid-feedback">{{ form.errors[id] }}</div>
+</template>
+
+<style scoped>
+
+</style>
