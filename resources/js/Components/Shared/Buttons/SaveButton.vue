992c432b8d6c186987cf31b40a1660de3e693c1b
Ahmed Bouzenada <ahmed.bouzenada@univ-constantine2.dz>
2024-03-28 17:23:29 +0100

    Added Save Button Component

---
+++ resources/js/Components/Shared/Buttons/SaveButton.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+    form: Object,
+    text: String
+})
+</script>
+
+<template>
+    <button type="submit" class="btn btn-success" :disabled="form.processing"><i
+        class="fa-solid fa-floppy-disk me-2"></i>{{ text }}
+    </button>
+</template>
+
+<style scoped>
+
+</style>
