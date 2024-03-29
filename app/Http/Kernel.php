4d865a3e0ffa8d2743ef3e5d2c9adef47f52424d
Ahmed Bouzenada <ahmed.bouzenada@univ-constantine2.dz>
2024-03-29 20:08:02 +0100

    Breeze installed

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,7 +37,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            HandleInertiaRequests::class,
+            \App\Http\Middleware\HandleInertiaRequests::class,
+            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ],
 
         'api' => [
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,16 @@
 import './bootstrap';
-import {createApp, h} from 'vue'
-import {createInertiaApp} from '@inertiajs/vue3'
-import Layout from "@/Components/Shared/Layout.vue";
+import '../css/app.css';
+import {createApp, h} from 'vue';
+import {createInertiaApp} from '@inertiajs/vue3';
+import {ZiggyVue} from '../../vendor/tightenco/ziggy';
 import 'bootstrap';
 import '@popperjs/core';
+import Layout from "@/Components/Shared/Layout.vue";
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    progress: {
-        delay: 250,
-        color: 'red',
-        includeCSS: true,
-        showSpinner: false,
-    },
+    title: (title) => `${title} - ${appName}`,
     resolve: name => {
         const pages = import.meta.glob('./Pages/**/*.vue', {eager: true})
         let page = pages[`./Pages/${name}.vue`]
@@ -21,6 +20,7 @@ createInertiaApp({
     setup({el, App, props, plugin}) {
         const app = createApp({render: () => h(App, props)})
             .use(plugin)
+            .use(ZiggyVue)
         app.config.globalProperties.$filters = {
             currency(value) {
                 return 'DZD ' + new Intl.NumberFormat('ar-DZ').format(value);
@@ -28,6 +28,10 @@ createInertiaApp({
         }
         app.mount(el);
     },
-    title: title => `MoneyMaster - ${title}`
-})
-
+    progress: {
+        delay: 250,
+        color: 'red',
+        includeCSS: true,
+        showSpinner: false,
+    },
+});
