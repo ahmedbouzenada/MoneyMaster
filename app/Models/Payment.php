85f678cb85b7594a95492ac648da94fb7ccae7dd
Ahmed Bouzenada <ahmed.bouzenada@univ-constantine2.dz>
2024-03-31 16:05:30 +0100

    Cleaning Unused Components

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -2,10 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Carbon;
 
 class Payment extends Model
 {
--- resources/js/Components/PrimaryButton.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <button
-        class="btn btn-dark btn-sm text-uppercase fw-semibold"
-    >
-        <slot/>
-    </button>
-</template>
